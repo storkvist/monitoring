3dab2f1dc74e9d61d50404896504c8ea79bcd991
Vitaly Shlyaga <storkvist@storkvist.net>
2013-01-23 17:06:51 +0400

    Assets Pipeline

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
@@ -31,6 +31,7 @@ gem 'jquery-rails'
 
 group :development do
   gem 'capistrano'
+  gem 'rvm-capistrano'
 end
 
 # To use ActiveModel has_secure_password
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,10 @@
 # encoding: utf-8
 
+require 'rvm/capistrano'
 require 'bundler/capistrano'
+load 'deploy/assets'
+
+set :rvm_type, :system
 
 set :application, 'monitoring.mgup.local'
 
@@ -12,6 +16,9 @@ set :deploy_to, '/var/monitoring'
 
 set :user, 'root'
 set :use_sudo, false
+default_run_options[:pty] = true
+
+set :normalize_asset_timestamps, false
 
 # Очистка старых релизов после каждого деплоя.
 after 'deploy:restart', 'deploy:cleanup'
@@ -30,10 +37,10 @@ namespace :deploy do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
 
-  desc 'Копирование файла database.yml в новый релиз.'
-  task :copy_in_database_yml do
-    run "cp #{shared_path}/config/database.yml #{latest_release}/config/"
-  end
+  #desc 'Копирование файла database.yml в новый релиз.'
+  #task :copy_in_database_yml do
+  #  run "cp #{shared_path}/config/database.yml #{latest_release}/config/"
+  #end
 end
 
-before 'deploy:assets:precompile', 'deploy:copy_in_database_yml'
+#before 'deploy:assets:precompile', 'deploy:copy_in_database_yml'
